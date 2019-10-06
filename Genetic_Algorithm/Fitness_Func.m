3f7939c3a0a9d5a49812a50a277f01d12e0ce8b7
VineetKhurana1708 <VineetKhurana1708@gmail.com>
2019-10-06 01:19:16 -0700

    Add files via upload

--- Genetic_Algorithm/Fitness_Func.m
+++ Genetic_Algorithm/Fitness_Func.m
@@ -1,11 +1,16 @@
-function Fit_Val = Fitness_Func(Chromosome,freq) 
+function Fit_Val = Fitness_Func(Chromosome,freq)
 
 
 MAT=Converttoint(Chromosome);
 
-NEC_DATA= NEC(MAT,freq);
+[error,NEC_DATA]= NEC(MAT,freq);
 
+if(error==0)
+    Fit_Val=max(NEC_DATA{1,6});% Max of AR
+end
 
+if(error==1)
+    Fit_Val=0;
+end
 
-Fit_Val=max(NEC_DATA{1,6});% Sum of AR
-end 
+end
