aa402c2e725af76c6bf7ff33d2b83e20710e033c
VineetKhurana1708 <VineetKhurana1708@gmail.com>
2019-10-06 01:18:45 -0700

    Add files via upload

--- MATLAB_NEC_INTERFACE/NEC.m
+++ MATLAB_NEC_INTERFACE/NEC.m
@@ -1,5 +1,5 @@
-%Data will be stored in cell array Imported Data  after running this  
-function Imported_Data= NEC(MAT,freq)%in Mhz
+%Data will be stored in cell array Imported Data  after running this
+function [error,Imported_Data]= NEC(MAT,freq)%in Mhz
 
 
 % Assumption: When all 5 bits are 1 , it corresponds to 0.5lambda , Source : Linden
@@ -7,12 +7,12 @@ function Imported_Data= NEC(MAT,freq)%in Mhz
 
 
 
-%Important: Code not working at higher frequencies due to loss of precision . Change to cm . 
+%Important: Code not working at higher frequencies due to loss of precision . Change to cm .
 
 
 
 
-N_Seg= 20 ;% Number of segments per wavelength , recommended 20 in NEC 
+N_Seg= 20 ;% Number of segments per wavelength , recommended 20 in NEC
 lambda = 300/freq;%in metres
 
 
@@ -25,24 +25,24 @@ input_vec(:,:)=0;
 input_vec(2:ln,2:4)=MAT(1:(ln-1),1:3);
 input_vec(:,5:7)=MAT(:,1:3);
 
- input_vec=Autosegment(input_vec);
+input_vec=Autosegment(input_vec);
 
- 
- 
-%Mapping 
- a=0.016*lambda;
- b=lambda/2;
- input_vec(2:ln,2:7) = (input_vec(2:ln,2:7))*(b-a)/(31) + a;
-  input_vec(1,5:7) = (input_vec(1,5:7))*(b-a)/(31) + a;
- 
+
+
+%Mapping
+a=0.016*lambda;
+b=lambda/2;
+input_vec(2:ln,2:7) = (input_vec(2:ln,2:7))*(b-a)/(31) + a;
+input_vec(1,5:7) = (input_vec(1,5:7))*(b-a)/(31) + a;
+
 
 filename = 'buffer';
 %input_vec= [0 0 0 -0.1 0 20 ;-0.1 0 20 1 0 20;1 0 20 16 0 50]; % Format is [x1,y1,z1,x2,y2,z2]in one row . Resembles inp file format
 
-fid= fopen(strcat(filename,'.inp'),'w'); 
+fid= fopen(strcat(filename,'.inp'),'w');
 
 
-formatSpec = "CM Ex card is correct, errors can be bugs \r\nCE \r\nGW 1 %d %d %d %d %d %d %d  0.01e-3\r\n" + "GW 2 %d %d %d %d %d %d %d  0.01e-3\r\n"+"GW 3 %d %d %d %d %d %d %d  0.01e-3\r\n"+"GW 4 %d %d %d %d %d %d %d  0.01e-3\r\n"+"GW 5 %d %d %d %d %d %d %d  0.01e-3\r\n"+"GW 6 %d %d %d %d %d %d %d  0.01e-3\r\n"+"GW 7 %d %d %d %d %d %d %d  0.01e-3\r\n"+"GE 1\r\nGN 1 0\r\nEX 0 1 1 0 1 0\r\nFR 0 1 0 0 %d 0\r\nRP 0 19 73 1003 -90 0 5 5 ";
+formatSpec = "CM Ex card is correct, errors can be bugs \r\nCE \r\nGW 1 %d %d %d %d %d %d %d  1e-3\r\n" + "GW 2 %d %d %d %d %d %d %d  1e-3\r\n"+"GW 3 %d %d %d %d %d %d %d  1e-3\r\n"+"GW 4 %d %d %d %d %d %d %d  1e-3\r\n"+"GW 5 %d %d %d %d %d %d %d  1e-3\r\n"+"GW 6 %d %d %d %d %d %d %d  1e-3\r\n"+"GW 7 %d %d %d %d %d %d %d  1e-3\r\n"+"GE 1\r\nGN 1 0\r\nEX 0 1 1 0 1 0\r\nFR 0 1 0 0 %d 0\r\nRP 0 19 73 1003 -90 0 5 5 ";
 
 
 input_vec_trans=input_vec';
@@ -54,45 +54,45 @@ fprintf(fid,str);
 fclose(fid);
 
 
-
-system(strcat('nec2++ -i ',strcat(filename,'.inp'))); % USes arg filename to call NEC .For example , system('nec2++ -i buff.inp')
-
-
-fid=-1;% Sometimes MATLAB was unable to open this file when called after NEC , maybe because NEC hadnt finished processing the output yet . Hence this loop
-while(fid==-1)
-fid=fopen(strcat(filename,'.out'));
-%pause(2);
+error=0;
+error=system(strcat('nec2++ -i ',strcat(filename,'.inp'))); % USes arg filename to call NEC .For example , system('nec2++ -i buff.inp')
+Imported_Data=[];%Empty matrix needs to be returned if error
+if(error==0)
+    
+    fid=-1;% Sometimes MATLAB was unable to open this file when called after NEC , maybe because NEC hadnt finished processing the output yet . Hence this loop
+    while(fid==-1)
+        fid=fopen(strcat(filename,'.out'));
+        %pause(2);
+    end
+    
+    pattern = "RADIATION PATTERNS";
+    
+    
+    while(1)
+        line_ex = fgetl(fid);
+        
+        if(contains(line_ex,pattern))
+            break;
+        end
+    end
+    
+    
+    Imported_Data = textscan(fid,'%f %f %f %f %f %f %f %s %f %f %f %f','headerlines',3);%data starts after 3 field after Radiation pattern string appears
+    
+    
+    for i= 1:1:length(Imported_Data{1})%This post processsing is taking most of the time in the program , optimise this
+        buff=isnan(str2double(Imported_Data{8}(i)));
+        if(buff==0)
+            Imported_Data{12}(i)=Imported_Data{11}(i);
+            Imported_Data{11}(i)=Imported_Data{10}(i);
+            Imported_Data{10}(i)=Imported_Data{9}(i);
+            Imported_Data{9}(i)=str2double(Imported_Data{8}(i));
+            Imported_Data{8}(i)=cellstr(" ");
+        end
+    end
+    
+    
+    fclose(fid);
+    
 end
-
-
-skip=0;
-pattern = "RADIATION PATTERNS";
-
-
- while(1)
- line_ex = fgetl(fid);
- 
- if(contains(line_ex,pattern))
-  break;   
- end
- skip=skip+1;
- end
- 
- 
- Imported_Data = textscan(fid,'%f %f %f %f %f %f %f %s %f %f %f %f','headerlines',3);%data starts after 3 field after Radiation pattern string appears
- 
- 
-for i= 1:1:length(Imported_Data{1})%This post processsing is taking most of the time in the program , optimise this 
-buff=isnan(str2double(Imported_Data{8}(i)));
-if(buff==0)
-Imported_Data{12}(i)=Imported_Data{11}(i);
-Imported_Data{11}(i)=Imported_Data{10}(i);
-Imported_Data{10}(i)=Imported_Data{9}(i);
-Imported_Data{9}(i)=str2double(Imported_Data{8}(i));
-Imported_Data{8}(i)=cellstr(" ");
-end
-end
-
-
-fclose(fid);
 end
